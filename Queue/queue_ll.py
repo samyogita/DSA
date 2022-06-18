a9e250c4755e62a2e49604c557ccf9af224f056c
samyogita <bhandarisamyogita@gmail.com>
2022-06-18 21:44:52 +0530

    enqueue operation in queue

---
+++ Queue/queue_ll.py
@@ -0,0 +1,45 @@
+from multiprocessing.sharedctypes import Value
+
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next= None
+
+    def __str__(self):
+        return str(self.value)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        curNode = self.head
+        while curNode:
+            yield curNode
+            curNode = curNode.next
+
+class Queue:
+    def __init__(self):
+        self.LinkedList = LinkedList()
+
+    def _str__(self):
+        values = [str(x) for x in self.LinkedList]
+        return ' '.join(values)
+
+    
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.LinkedList.head == None:
+            self.LinkedList.head = newNode
+            self.LinkedList.tail = newNode
+        else:
+            self.LinkedList.tail.next = newNode
+            self.LinkedList.tail = newNode
+
+customQueue = Queue()
+customQueue.enqueue(1)
+customQueue.enqueue(2)
+customQueue.enqueue(3)
+print(customQueue)
