51efbe3716ec5adb6fbb8a3b80cb704e7a3847a8
samyogita <bhandarisamyogita@gmail.com>
2022-06-24 23:24:14 +0530

    searching a node in binary tree

--- Trees/binary_tress.py
+++ Trees/binary_tress.py
@@ -63,6 +63,28 @@ def levelOrderTraversal(rootNode):
 
 levelOrderTraversal(newBT)
 
+def searchBT(rootNode, nodeValue):
+    if not rootNode:
+        return "The BT does not exist"
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not (customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value.data == nodeValue:
+                return "Success"
+            
+            if (root.value.leftChild is not None):
+                customQueue.enqueue(root.value.leftChild)
+            
+            if (root.value.rightChild is not None):
+                customQueue.enqueue(root.value.rightChild)
+        return "Not found"
+
+print(searchBT(newBT, "Tea"))
+            
+
+
 
 
 
