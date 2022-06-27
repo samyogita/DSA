8a56ebe4d1213cd2d521fa5e63d60b70e298e9e9
samyogita <bhandarisamyogita@gmail.com>
2022-06-27 11:39:56 +0530

    inserting nodes in a binary tree

--- Trees/binarytree_list.py
+++ Trees/binarytree_list.py
@@ -4,4 +4,15 @@ class BinaryTree:
         self.lastUsedIndex = 0
         self.maxSize = size
 
+    def insertNode(self, value):
+        if self.lastUsedIndex + 1 == self.maxSize:
+            return "Binary tree is full"
+        self.customList[self.lastUsedIndex+1] = value
+        self.lastUsedIndex += 1
+        return "The value has been inserted successfully"
+
+
 newBT = BinaryTree(8)
+print(newBT.insertNode("Drinks"))
+print(newBT.insertNode("Hot"))
+print(newBT.insertNode("Cold"))
