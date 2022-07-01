6036836e107f71600264b728b823d6f3cba51529
samyogita <bhandarisamyogita@gmail.com>
2022-07-01 19:12:47 +0530

    preorder, inorder and post order traversal of AVL binary tree

---
+++ AVLTrees/AVL.py
@@ -0,0 +1,30 @@
+class AVLNode:
+    def __init__(self, data):
+        self.data = data 
+        self.leftChild = None
+        self.rightChild = None
+        self.height =  1
+
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+           
+newAVL = AVLNode(10)
