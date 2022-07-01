0f7d2df504f36893e2e213e5da2812ab99521ec0
samyogita <bhandarisamyogita@gmail.com>
2022-07-01 16:14:38 +0530

    in order traversal of a binary search tree

--- BinarySearchTree/binarysearchtree.py
+++ BinarySearchTree/binarysearchtree.py
@@ -32,6 +32,12 @@ def preOrderTraversal(rootNode):
     preOrderTraversal(rootNode.leftChild)
     preOrderTraversal(rootNode.rightChild)
 
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
 
 
 newBST = BSTNode(None)
@@ -44,4 +50,5 @@ print(insertNode(newBST, 80))
 print(insertNode(newBST, 100))
 print(insertNode(newBST, 20))
 print(insertNode(newBST, 40))
-preOrderTraversal(newBST)
+#preOrderTraversal(newBST)
+inOrderTraversal(newBST)
