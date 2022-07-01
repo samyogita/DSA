ce5da47cfe181979ae6ac4c34bd0ef6c11d94c09
samyogita <bhandarisamyogita@gmail.com>
2022-07-01 16:04:13 +0530

    inserting an element in a binary  search tree

--- BinarySearchTree/binarysearchtree.py
+++ BinarySearchTree/binarysearchtree.py
@@ -1,7 +1,32 @@
+from logging import root
+from platform import node
+
+
 class BSTNode:
     def __init__(self, data):
-        self.data = None
+        self.data = data
         self.leftChild = None
         self.rightChild = None
 
+def insertNode(rootNode, nodeValue):
+    if rootNode.data == None:
+            rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.rightChild, nodeValue)
+    
+    return "The node has been successfully inserted"
+
+
 newBST = BSTNode(None)
+print(insertNode(newBST, 70))
+print(insertNode(newBST, 60))
+print(newBST.data)
+print(newBST.leftChild.data)
