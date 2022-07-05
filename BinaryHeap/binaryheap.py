555cb450e750c52e0bbe7e9e7b1ebf832c59f4b8
samyogita <bhandarisamyogita@gmail.com>
2022-07-05 16:12:19 +0530

    finding the size of the binary heap

--- BinaryHeap/binaryheap.py
+++ BinaryHeap/binaryheap.py
@@ -1,6 +1,6 @@
 class Heap:
     def __init__(self, size):
-        self.customList = (size+1) * None
+        self.customList = (size+1) * [None]
         self.heapSize = 0
         self.maxSize = size + 1
 
@@ -10,4 +10,12 @@ def peekofHeap(rootNode):
     else:
         return rootNode.customList[1]
 
+def sizeofHeap(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.heapSize
+
+
 newBinaryHeap = Heap(5)
+print(sizeofHeap(newBinaryHeap))
