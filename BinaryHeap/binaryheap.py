3812e0760e7f3daa216eb3a1d65e8c928ce53a30
samyogita <bhandarisamyogita@gmail.com>
2022-07-05 16:25:11 +0530

    level order traversal of a binary heap

--- BinaryHeap/binaryheap.py
+++ BinaryHeap/binaryheap.py
@@ -16,6 +16,13 @@ def sizeofHeap(rootNode):
     else:
         return rootNode.heapSize
 
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1, rootNode.heapSize+1):
+            print(rootNode.customList[i])
+
 
 newBinaryHeap = Heap(5)
 print(sizeofHeap(newBinaryHeap))
