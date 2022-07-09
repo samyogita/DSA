47aaad42e356eb89fa82a6294178bb68daeff314
samyogita <bhandarisamyogita@gmail.com>
2022-07-09 23:09:06 +0530

    serching a string in trie

--- Trie/trie.py
+++ Trie/trie.py
@@ -19,8 +19,22 @@ class Trie:
             current = node
         current.endOfString = True
         print("Successfully inserted")
+    
+    def searchString(self, word):
+        currentNode = self.root
+        for i in word:
+            node = currentNode.children.get(i)
+            if node == None:
+                return False
+            currentNode = node
+
+        if currentNode.endOfString == True:
+            return True
+        else:
+            return False
 
     
 newTrie = Trie()
 newTrie.insertString("App")
 newTrie.insertString("Apple")
+print(newTrie.searchString("App"))
