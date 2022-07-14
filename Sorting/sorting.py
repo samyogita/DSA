a8cf643f9b0fb86215c983a50e2ccc37cb1aa046
samyogita <bhandarisamyogita@gmail.com>
2022-07-14 21:53:04 +0530

    insertion sort

--- Sorting/sorting.py
+++ Sorting/sorting.py
@@ -16,8 +16,18 @@ def selectionSort(customList):
             
     print(customList)
 
+def insertionSort(customList):
+    for i in range(1, len(customList)):
+        key = customList[i]
+        j = i - 1
+        while j>=0 and key < customList[j]:
+            customList[j+1] = customList[j]
+            j -= 1
+        customList[j+1] = key
+    print(customList)
 
 
 cList = [2, 1, 7, 6, 5, 3, 4, 9, 8]
 #bubbleSort(cList)
 selectionSort(cList)
+insertionSort(cList)
