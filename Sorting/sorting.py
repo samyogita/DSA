0a6ba0d18137292f26a0538b4985a40317606d04
samyogita <bhandarisamyogita@gmail.com>
2022-07-15 23:16:57 +0530

    merge function

--- Sorting/sorting.py
+++ Sorting/sorting.py
@@ -51,7 +51,39 @@ def bucketSort(customList):
     
     return customList
 
+def merge(customList, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
 
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(0, n1):
+        L[i] = customList[l+i]
+    
+    for j in range(0, n2):
+        R[j] = customList[m+1+j]
+    
+    i = 0 
+    j = 0
+    k = l
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            customList[k] = L[i]
+            i += 1
+        else:
+            customList[k] = R[j]
+            j += 1
+        k += 1
+    while i < n1:
+        customList[k] = L[i]
+        i += 1
+        k += 1
+    
+    while j < n2:
+        customList[k] = R[j]
+        j += 1
+        k += 1
     
 
 
