128336156f6926f6cf68ea5727fabc3f032916ca
samyogita <bhandarisamyogita@gmail.com>
2022-07-17 21:47:33 +0530

    heapify function

--- Sorting/sorting.py
+++ Sorting/sorting.py
@@ -109,6 +109,21 @@ def quickSort(customList, low, high):
         quickSort(customList, low, pi-1)
         quickSort(customList, pi+1, high)
 
+def heapify(customList, n, i):
+    smallest = i
+    l = 2*i + 1
+    r = 2*i + 2
+    if l < n and customList[l] < customList[smallest]:
+        smallest = l
+    
+    if r < n and customList[r] < customList[smallest]:
+        smallest = r
+    
+    if smallest != i:
+        customList[i], customList[smallest] = customList[smallest], customList[i]
+        heapify(customList, n, smallest)
+
+
 cList = [2, 1, 7, 6, 5, 3, 4, 9, 8]
 #bubbleSort(cList)
 #selectionSort(cList)
