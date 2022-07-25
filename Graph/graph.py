4497b83b8c28f71ed86109f9ec14cab8619b039e
samyogita <bhandarisamyogita@gmail.com>
2022-07-25 23:00:08 +0530

    bfs traversal in a graph

--- Graph/graph.py
+++ Graph/graph.py
@@ -7,6 +7,18 @@ class Graph:
     
     def addEdge(self, vertex, edge):
         self.gdict[vertex].append(edge)
+    
+    def bfs(self, vertex):
+        visited = [vertex]
+        queue = [vertex]
+        while queue:
+            deVertex = queue.pop(0)
+            print(deVertex)
+            for adjacentVertex in self.gdict[deVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    queue.append(adjacentVertex)
+    
 
 
 customDict = {"a" : ["b", "c"],
